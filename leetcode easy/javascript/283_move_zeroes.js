677ce0856be660b95d2b8682d96dfb1145d80c7b
splimon <splimon@hawaii.edu>
2025-06-18 07:23:11 -1000

    Update 283_move_zeroes.js

--- leetcode easy/javascript/283_move_zeroes.js
+++ leetcode easy/javascript/283_move_zeroes.js
@@ -25,7 +25,6 @@ function moveZeros(nums) {
             left++; // increment left pointer
         }
     }
-    return nums;
 };
 
 console.log(moveZeros([0, 1, 0, 3, 12])); // output: [ 1, 3, 12, 0, 0 ]
